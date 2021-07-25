e6877ce08943045c48223c19ae350244ba64d5a0
ShadiFgh <shadifarokhghate@gmail.com>
2021-07-25 18:28:47 +0430

    LU decomposition done

--- main.py
+++ main.py
@@ -32,3 +32,23 @@ if IsPositiveDefinite(matrix) == True:
     print('This matrix is positive definite.')
 else:
     print('This matrix is not positie definie.')
+
+
+def LU_decomposition(A):
+
+    n = len(A)
+    L = [[0.0] * n for i in range(n)]
+    U = [[0.0] * n for i in range(n)]
+
+    for j in range(n):
+        L[j][j] = 1.0
+
+        for i in range(j+1):
+            s1 = sum(U[k][j] * L[i][k] for k in range(i))
+            U[i][j] = A[i][j] - s1
+
+        for i in range(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in range(j))
+            L[i][j] = (A[i][j] - s2) / U[j][j]
+
+    return (L, U)
